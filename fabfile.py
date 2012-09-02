858193918e0423952ac82ba50bfaed96b450fdd6
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-09-01 23:18:29 -0300

    initial directory structure

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
+
 def _sh(command):
     subprocess.Popen(command.split())
 
 
 def tests():
     _sh('python -m unittest discover . \'*_test.py\'')
+    # _sh('sensible-browser ./templates/tests.html')
---
+++ static/tests/sample_test.js
@@ -0,0 +1,3 @@
+test( 'simple test', function() {
+  equal(1 + 1, 2, "ok" );
+});
