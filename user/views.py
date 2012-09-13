fbd5d47abb0ae87e0ab581e2dda5d3d61632865d
André Casimiro <ah.casimiro@gmail.com>
2012-09-13 20:41:12 -0300

    Login with facebook working
    TODO:
      - randomize and check state in session

--- user/views.py
+++ user/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import requests
-from flask import Blueprint, render_template
+from werkzeug.urls import Href, url_decode
+
+from flask import Blueprint
+from flask import request
+from flask import render_template
 from flask import url_for, redirect
-from werkzeug.urls import Href
 
 app = Blueprint('user', 'user')
 
@@ -13,25 +16,50 @@ def login():
 
 ########## FABEBOOK LOGIN ##########
 '''INFO: https://developers.facebook.com/docs/authentication/server-side/'''
+FACEBOOK_APP_ID = '186391648162058'
+FACEBOOK_APP_SECRET = 'd6855cacdb51225519e8aa941cf7cfee'
 
 @app.route('/login/facebook')
 def login_facebook():
     href = Href('https://www.facebook.com/dialog/oauth')
     params = {
         # client_id: app id from facebook
-        'client_id': '186391648162058',
         # redirect_uri: where the user will be redirected to
-        'redirect_uri': url_for('user.facebook_authorized', _external=True),
         # scope: comma separated list of requested permissions
-        'scope': 'email',
         # state: some arbitrary but unique string. Used to prevent from CSRF.
-        'state': 'lalala',
+        'client_id': FACEBOOK_APP_ID,
+        'redirect_uri': url_for('user.facebook_authorized', _external=True),
+        'scope': 'email',
+        'state': 'abebubaba',  # TODO: randomize and save state in session
     }
     href = href(**params)
     return redirect(href)
 
 @app.route('/login/facebook/authorized')
 def facebook_authorized():
-    return "facebook authorized!"
+    # TODO: verifiy request.args.get('state') saved in session
+    href = Href('https://graph.facebook.com/oauth/access_token')
+    params = {
+        # client_id: app id from facebook
+        # client_secret: app secret from facebook
+        # code: code given by facebook to exchange for an access_token
+        # redirect_uri: must be the same as the one in the login_facebook
+        'client_id': FACEBOOK_APP_ID,
+        'client_secret': FACEBOOK_APP_SECRET,
+        'code': request.args.get('code'),
+        'redirect_uri': url_for('user.facebook_authorized', _external=True),
+    }
+    href = href(**params)
+    data = url_decode(requests.get(href).content)
+    access_token = data['access_token']
+
+    href = Href('https://graph.facebook.com/')
+    href = href('me', fields='email,name', access_token=access_token)
+    
+    # TODO: create user using name and email
+    # TODO: persist access_token and expiration in DB
+    
+    return requests.get(href).content
+    return render_template('home.html', email=)
 
 ####################################
