c8ecb29b5d76a090c353fbc23784afe3722601b2
Luiz Armesto <luiz.armesto@gmail.com>
2012-09-13 19:16:46 -0300

    Adding Sinon.JS and 2 test examples

--- static/tests/sample_test.js
+++ static/tests/sample_test.js
@@ -1,11 +1,24 @@
 (function() {
 
   define(['sample'], function(sample) {
-    test('simple test', function() {
-      return equal(1 + 1, 2);
+    module('simple examples');
+    test('1 + 1', function() {
+      expect(1);
+      return equal(1 + 1, 2, 'Should be equal to 2');
     });
-    return test('sample adder', function() {
-      return equal(sample.adder(1, 2), 3);
+    module('sample module');
+    test('adder', function() {
+      expect(1);
+      return equal(sample.adder(1, 2), 3, 'Should return 3 when add 1 and 2');
+    });
+    return test('callbacker (using spy)', function() {
+      var HEY, cb;
+      expect(2);
+      HEY = 'Hey!';
+      cb = sinon.spy();
+      sample.callbacker(cb, HEY);
+      ok(cb.calledOnce, 'Should be called once');
+      return ok(cb.calledWith(HEY), 'Should be called with "Hey!" string');
     });
   });
 
