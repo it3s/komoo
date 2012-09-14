9d44a62b9e0e2ac0c0ea1deb10b1c1a97caae2b8
André Casimiro <ah.casimiro@gmail.com>
2012-09-14 13:37:28 -0300

    working on google login

--- user/views.py
+++ user/views.py
@@ -53,14 +53,14 @@ def load_user(userid):
 FACEBOOK_APP_ID = '186391648162058'
 FACEBOOK_APP_SECRET = 'd6855cacdb51225519e8aa941cf7cfee'
 
-@app.route('/login/facebook')
+@app.route('/login/facebook/')
 def login_facebook():
     href = Href('https://www.facebook.com/dialog/oauth')
     csrf_token = randstr(10)
     params = {
-        # client_id: app id from facebook
-        # redirect_uri: where the user will be redirected to
-        # scope: comma separated list of requested permissions
+        # client_id: app id from provider.
+        # redirect_uri: where the user will be redirected to.
+        # scope: comma separated list of requested permissions.
         # state: some arbitrary but unique string. Used to prevent from CSRF.
         'client_id': FACEBOOK_APP_ID,
         'redirect_uri': url_for('user.facebook_authorized', _external=True),
@@ -71,7 +71,7 @@ def login_facebook():
     href = href(**params)
     return redirect(href)
 
-@app.route('/login/facebook/authorized')
+@app.route('/login/facebook/authorized/')
 def facebook_authorized():
     csrf_token = request.args.get('state', None)
     if not csrf_token or csrf_token != session['state']:
@@ -84,10 +84,10 @@ def facebook_authorized():
 
     href = Href('https://graph.facebook.com/oauth/access_token')
     params = {
-        # client_id: app id from facebook
-        # client_secret: app secret from facebook
-        # code: code given by facebook to exchange for an access_token
-        # redirect_uri: must be the same as the one in the login_facebook
+        # client_id: app id from provider.
+        # client_secret: app secret from provider.
+        # code: code given by provider to exchange for an access_token.
+        # redirect_uri: must be the same as the one in step 1.
         'client_id': FACEBOOK_APP_ID,
         'client_secret': FACEBOOK_APP_SECRET,
         'code': request.args.get('code'),
@@ -111,7 +111,73 @@ def facebook_authorized():
 ####################################
 
 
-@app.route('/secret')
+########## GOOGLE LOGIN ##########
+'''INFO: 
+
+STEP 1 - Getting authorization from the user
+  https://developers.google.com/accounts/docs/OAuth2Login
+
+STEP 2 - Exchange the authorization code for an access_token
+  https://developers.google.com/accounts/docs/OAuth2WebServer
+
+STEP 3 - Accessing the API
+'''
+GOOGLE_APP_ID = '806210666216.apps.googleusercontent.com'
+GOOGLE_APP_SECRET = 'wWFaxUXs3C7atzHThA-YD8T3'
+
+@app.route('/login/google/')
+def login_google():
+    href = Href('https://accounts.google.com/o/oauth2/auth')
+    csrf_token = randstr(10)
+    params = {
+        # client_id: app id from google.
+        # redirect_uri: where the user will be redirected to.
+        # scope: space separated list of requested permissions.
+        # state: some arbitrary but unique string. Used to prevent from CSRF.
+        # response_type: 'code' or 'token', depends on the application type.
+        'client_id': GOOGLE_APP_ID,
+        'redirect_uri': url_for('user.google_authorized', _external=True),
+        'scope': 'https://www.googleapis.com/auth/userinfo.profile '  # space separated
+                 'https://www.googleapis.com/auth/userinfo.email',
+        'state': csrf_token,
+        'response_type': 'code'
+    }
+    session['state'] = csrf_token
+    href = href(**params)
+    return redirect(href)
+
+@app.route('/login/google/authorized/')
+def google_authorized():
+    csrf_token = request.args.get('state', None)
+    if not csrf_token or csrf_token != session['state']:
+        abort(403)  # csrf attack! get that bastard!
+
+    error = request.args.get('error', None)
+    if error:
+        error_description = request.args.get('error_description', None)
+        return render_template("user/login.html", error_msg=error_description)
+
+    href = 'https://accounts.google.com/o/oauth2/auth'
+    params = {
+        # client_id: app id from provider.
+        # client_secret: app secret from provider.
+        # code: code given by provider to exchange for an access_token.
+        # redirect_uri: must be the same as the one in step 1.
+        # grant_type: just to meet the OAuth 2.0 specification.
+        'client_id': GOOGLE_APP_ID,
+        'client_secret': GOOGLE_APP_SECRET,
+        'code': request.args.get('code'),
+        'redirect_uri': url_for('user.google_authorized', _external=True),
+        'grant_type': 'authorization_code'
+    }
+    return requests.post(href, data=params).content  # google requires POST, not GET
+    
+
+
+##################################
+
+
+@app.route('/secret/')
 @login_required
 def secret():
     return render_template('user/secret.html')
